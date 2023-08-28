f3d47667c323ab5a123d1aa4873122b78d17fc14
Furkan Karakus <furkankarakus01@hotmail.com>
2023-08-29 00:00:39 +0300

    my own package install

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import {ThemeProvider} from './context/ThemeContext';
 import Container from './components/Container';
 import { UserProvider } from './context/UserContext';
+import { Button, ExampleComponent } from 'yummys';
+import "yummys/dist/index.css"
 function App() {
   return (
     <ThemeProvider>
-
+  <Button text="Hello"/>
+  <ExampleComponent text="Test"/>
      <UserProvider >
      <Container />
+   
      </UserProvider>
      
     </ThemeProvider>
