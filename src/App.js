f1ad3c7f37dd852aa1ecabd8bafa1006b12bd941
Furkan Karakus <furkankarakus01@hotmail.com>
2023-09-28 20:19:57 +0300

    changed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import {ThemeProvider} from './context/ThemeContext';
 import Container from './components/Container';
 import { UserProvider } from './context/UserContext';
-import { Button, ExampleComponent } from 'yummys';
+import { Button, Text, Title } from 'yummys';
 import "yummys/dist/index.css"
 function App() {
   return (
     <ThemeProvider>
   <Button text="Hello"/>
-  <ExampleComponent text="Test"/>
+  <Text text="New Version"/>
+  <Title text="Say Hello To v1.1.0"/>
      <UserProvider >
      <Container />
    
